db15d6898c4b530e9c3a7f7c593b536f3c2d164d
Sylviane Farooq <sylvianepinto@hotmail.fr>
2023-04-16 12:01:19 +0200

    Implement helper/Date test

--- src/helpers/Date/index.test.js
+++ src/helpers/Date/index.test.js
@@ -2,13 +2,17 @@
  * 
  */
 
+import { getMonth } from "./index";
+
 describe("Date helper", () => {
     describe("When getMonth is called", () => {
         it("the function return janvier for 2022-01-01 as date", () => {
-            // to implement
+            const month = getMonth(new Date("2022-01-01"));
+            expect(month).toBe("janvier");
         });
         it("the function return juillet for 2022-07-08 as date", () => {
-            // to implement
+            const month = getMonth(new Date("2022-07-08"));
+            expect(month).toBe("juillet");            
         });
     });
 })
